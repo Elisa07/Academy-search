82220ef04c2d6314eeb5b29d775bd3312203c549
murae <e.mura@campus.unimib.it>
2021-07-21 11:52:10 +0200

    Checkbox in AddComponent

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -4,7 +4,6 @@ import {AuthenticationService} from "../services/authentication.service";
 import {faCheck, faTimesCircle} from "@fortawesome/free-solid-svg-icons";
 import { CookieService } from 'ngx-cookie-service';
 import {DataService, Result} from '../data.service';
-import {Subject, Subscription} from "rxjs";
 
 interface CheckBox {
   name: string, value: string, checked: boolean
@@ -23,8 +22,8 @@ export class AddComponent implements OnInit {
   isVisible = false;
   researches: Result[] = [];
   checkBox: CheckBox[] = []
-  private checkBoxSub = new Subject<CheckBox[]>();
   editMode = false;
+  allSelected = false;
 
   constructor(private authService: AuthenticationService, private cookieService: CookieService, private dataService: DataService) { }
 
@@ -34,11 +33,7 @@ export class AddComponent implements OnInit {
   }
 
   private initForm() {
-    this.checkBoxSub.subscribe((checkBoxesValues: CheckBox[]) => {
-      this.checkBox = checkBoxesValues;
-    });
     this.addForm = new FormGroup({
-      /*'id': new FormControl(null),*/
       'title': new FormControl(null, Validators.required),
       'description': new FormControl(null, Validators.required),
       'keys': new FormControl(null, Validators.required),
@@ -53,7 +48,9 @@ export class AddComponent implements OnInit {
       "chiavi": this.addForm.value.keys,
       "url": this.addForm.value.url
     }
-    this.dataService.addPost(postData);
+    this.dataService.addPost(postData).subscribe(() => {
+      this.onGetResearches();
+    });
     this.initForm();
     this.success();
 
@@ -74,23 +71,48 @@ export class AddComponent implements OnInit {
   onGetResearches() {
     this.dataService.getAllResearches().subscribe((researches) => {
       this.researches = researches;
-      console.log(this.researches);
+      this.checkBox = [];
       for (let i = 0; i < this.researches.length; i++) {
         this.checkBox.push({name: String(this.researches[i].id), value: String(this.researches[i].id), checked: false});
       }
     })
   }
 
-  onSelectAll() {
-    for (const c of this.checkBox){
-      c.checked = true;
+  onToggleSelection() {
+    if (!this.allSelected) {
+      for (const c of this.checkBox) {
+        c.checked = true;
+      }
     }
-
-    this.checkBoxSub.next(this.checkBox);
-
+    else {
+      for (const c of this.checkBox) {
+        c.checked = false;
+      }
+    }
+    this.allSelected = !this.allSelected;
   }
 
-  onToggleResearch(index: number) {
-    this.checkBox[index].checked = !this.checkBox[index].checked;
+  onDelete() {
+    let deleteIds: number[] = [];
+    for (let i = 0; i < this.checkBox.length; i++) {
+      if (this.checkBox[i].checked) {
+        deleteIds.push(this.researches[i].id);
+      }
+    }
+    this.dataService.removeResearches(deleteIds)
+      .subscribe(() => {
+        this.onGetResearches(); // Aggiorno l'interfaccia
+      });
+  }
+
+  closeEditMode() {
+    this.editMode = false;
+    this.resetCheckBox() // Tolgo tutti gli eventuali check selezionati
+  }
+
+  private resetCheckBox() {
+    for (let i = 0; i < this.checkBox.length; i++) {
+      this.checkBox[i].checked = false;
+    }
   }
 }
